313ac6f0ae539f068c5fb5fe7521c965470e4dec
Rasmus Graver <rasmus.graver@gmail.com>
2024-03-02 13:49:28 +0100

    Legger inn responsen som html objecter som synes

--- request.js
+++ request.js
@@ -2,6 +2,9 @@ const GetURL = "https://rasmusweb.no/get.php"
 const PostURL = "https://rasmusweb.no/post.json"
 
 async function getRequest() {
+  const getHtmlObj = document.getElementById("get")
+  getHtmlObj.innerHTML = "Waiting for response"
+
   const apiCallPromise = await fetch(GetURL, {
     method: "GET",
     //  mode: "no-cors", // no-cors, *cors, same-origin
@@ -9,19 +12,31 @@ async function getRequest() {
       Accept: "application/json",
     },
   })
+
+  getHtmlObj.innerHTML = ""
+
+  const p = document.createElement("p")
+  p.textContent = "StatusCodeOK: " + apiCallPromise.ok
+  getHtmlObj.appendChild(p)
+
+  // Getting the json entries from the response:
   const entries = await apiCallPromise.json()
   console.log(entries)
 
   for (const entry of entries) {
     console.log(entry)
+    const p = document.createElement("p")
+    p.textContent = "Entry: " + JSON.stringify(entry)
+    getHtmlObj.appendChild(p)
   }
+
+  // document.getElementById("get").innerHTML = obj.toString()
 
   //.then((response) => {
   //  console.log(response)
   //  console.log("OK: " + response.ok)
   // console.log(JSON.stringify(response.json))
   // const obj = JSON.parse(response)
-  // document.getElementById("get").innerHTML = obj.toString()
   // })
 }
 
