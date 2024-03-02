605ae7855fe0929fa1f15708b6968d575f798503
Rasmus Graver <rasmus.graver@gmail.com>
2024-03-02 17:16:39 +0100

    Added post test

--- request.js
+++ request.js
@@ -1,9 +1,9 @@
 const GetURL = "https://rasmusweb.no/get.php"
-const PostURL = "https://rasmusweb.no/post.json"
+const PostURL = "https://rasmusweb.no/post.php"
 
 async function getRequest() {
-  const getHtmlObj = document.getElementById("get")
-  getHtmlObj.innerHTML = "Waiting for response"
+  const htmlObj = document.getElementById("get")
+  htmlObj.innerHTML = "Waiting for response"
 
   const apiCallPromise = await fetch(GetURL, {
     method: "GET",
@@ -13,11 +13,11 @@ async function getRequest() {
     },
   })
 
-  getHtmlObj.innerHTML = ""
+  htmlObj.innerHTML = ""
 
   const p = document.createElement("p")
   p.textContent = "StatusCodeOK: " + apiCallPromise.ok
-  getHtmlObj.appendChild(p)
+  htmlObj.appendChild(p)
 
   // Getting the json entries from the response:
   const entries = await apiCallPromise.json()
@@ -27,21 +27,15 @@ async function getRequest() {
     console.log(entry)
     const p = document.createElement("p")
     p.textContent = "Entry: " + JSON.stringify(entry)
-    getHtmlObj.appendChild(p)
+    htmlObj.appendChild(p)
   }
-
-  // document.getElementById("get").innerHTML = obj.toString()
-
-  //.then((response) => {
-  //  console.log(response)
-  //  console.log("OK: " + response.ok)
-  // console.log(JSON.stringify(response.json))
-  // const obj = JSON.parse(response)
-  // })
 }
 
 async function postRequest() {
-  await fetch(PostURL, {
+  const htmlObj = document.getElementById("post")
+  htmlObj.innerHTML = "Waiting for response"
+
+  const apiCallPromise = await fetch(PostURL, {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -49,6 +43,18 @@ async function postRequest() {
     },
     body: JSON.stringify({ id: 78912 }),
   })
-    .then((response) => response.json())
-    .then((response) => console.log(JSON.stringify(response)))
+
+  htmlObj.innerHTML = ""
+
+  const p = document.createElement("p")
+  p.textContent = "StatusCodeOK: " + apiCallPromise.ok
+  htmlObj.appendChild(p)
+
+  // Getting the json from the response:
+  const responseJson = await apiCallPromise.json()
+  console.log(responseJson)
+
+  const p2 = document.createElement("p")
+  p2.textContent = "Response: " + responseJson
+  htmlObj.appendChild(p2)
 }
