7ed758dbfdd79e69631c18ec144986e45321e952
Rasmus Graver <rasmus.graver@gmail.com>
2024-03-02 13:41:57 +0100

    Skiftet fra .then syntax til await promise

--- request.js
+++ request.js
@@ -2,31 +2,38 @@ const GetURL = "https://rasmusweb.no/get.php"
 const PostURL = "https://rasmusweb.no/post.json"
 
 async function getRequest() {
-    const response = await fetch(GetURL, {
-        method: "GET",
-       //  mode: "no-cors", // no-cors, *cors, same-origin
-        headers: {
-            Accept: "application/json",
-        },
-    })
-        .then((response) => {
-            console.log( response )
-            console.log("OK: " + response.ok)
-            console.log(JSON.stringify(response.json))
-            // const obj = JSON.parse(response)
-            // document.getElementById("get").innerHTML = obj.toString()
-        })
+  const apiCallPromise = await fetch(GetURL, {
+    method: "GET",
+    //  mode: "no-cors", // no-cors, *cors, same-origin
+    headers: {
+      Accept: "application/json",
+    },
+  })
+  const entries = await apiCallPromise.json()
+  console.log(entries)
+
+  for (const entry of entries) {
+    console.log(entry)
+  }
+
+  //.then((response) => {
+  //  console.log(response)
+  //  console.log("OK: " + response.ok)
+  // console.log(JSON.stringify(response.json))
+  // const obj = JSON.parse(response)
+  // document.getElementById("get").innerHTML = obj.toString()
+  // })
 }
 
 async function postRequest() {
-    await fetch(PostURL, {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ id: 78912 }),
-    })
-        .then((response) => response.json())
-        .then((response) => console.log(JSON.stringify(response)))
+  await fetch(PostURL, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id: 78912 }),
+  })
+    .then((response) => response.json())
+    .then((response) => console.log(JSON.stringify(response)))
 }
