f12a00f246c15102b3140fce5594048bb0d3921d
Rasmus Graver <rasmus.graver@gmail.com>
2024-03-07 10:40:09 +0100

    La inn HS demo også

---
+++ hs.js
@@ -0,0 +1,59 @@
+const URL = "https://rasmusweb.no/hs.php"
+const GameID = "game3"
+
+async function getRequest() {
+    const htmlObj = document.getElementById("get_result")
+    htmlObj.innerHTML = "Waiting for response"
+
+    const apiCallPromise = await fetch(URL + "?id=" + GameID, {
+        method: "GET",
+        //  mode: "no-cors", // no-cors, *cors, same-origin
+        headers: {
+            Accept: "application/json",
+        },
+    })
+
+    htmlObj.innerHTML = ""
+    appendPElm(htmlObj, "StatusCodeOK: " + apiCallPromise.ok)
+
+    // Getting the json from the response (NOTE: Also await!)
+    const json = await apiCallPromise.json()
+    console.log(json)
+    appendPElm(htmlObj, "hs: " + json.hs)
+    appendPElm(htmlObj, "player: " + json.player)
+}
+
+// Poster ny HS til php backend
+async function postRequest(svar) {
+    const htmlObj = document.getElementById("post_result")
+    htmlObj.innerHTML = "Waiting for response"
+
+    postBody = {}
+    postBody.id = GameID
+    postBody.hs = document.getElementById("hs_number").value
+    postBody.player = document.getElementById("hs_player").value
+
+    const apiCallPromise = await fetch(URL, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+        },
+        body: JSON.stringify(postBody),
+    })
+
+    htmlObj.innerHTML = ""
+
+    appendPElm(htmlObj, "StatusCodeOK: " + apiCallPromise.ok)
+
+    // Getting the json from the response:
+    const responseJson = await apiCallPromise.json()
+    console.log(responseJson)
+
+    appendPElm(htmlObj, "Response: " + responseJson)
+}
+
+function appendPElm(htmlObj, text) {
+    const p = document.createElement("p")
+    p.textContent = text
+    htmlObj.appendChild(p)
+}
